22581715e8f7f6176896e72c0c0967c80bb6847c
Eduardo Saavedra <saavedra.edo@gmail.com>
2025-06-24 12:45:16 -0400

    change port

--- app.py
+++ app.py
@@ -219,4 +219,4 @@ if __name__ == '__main__':
         ]
         save_gallery_data(sample_data)
     
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=6000)
